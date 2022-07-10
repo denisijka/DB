cc526d967476c873eda297a39cb92759fef941cd
denisijka <37539630+denisijka@users.noreply.github.com>
2022-07-10 11:04:27 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1329,12 +1329,12 @@
                 }
             }
         }
-        function ssr_window_esm_isObject(obj) {
+        function isObject(obj) {
             return null !== obj && "object" === typeof obj && "constructor" in obj && obj.constructor === Object;
         }
         function extend(target = {}, src = {}) {
             Object.keys(src).forEach((key => {
-                if ("undefined" === typeof target[key]) target[key] = src[key]; else if (ssr_window_esm_isObject(src[key]) && ssr_window_esm_isObject(target[key]) && Object.keys(src[key]).length > 0) extend(target[key], src[key]);
+                if ("undefined" === typeof target[key]) target[key] = src[key]; else if (isObject(src[key]) && isObject(target[key]) && Object.keys(src[key]).length > 0) extend(target[key], src[key]);
             }));
         }
         const ssrDocument = {
@@ -4562,58 +4562,274 @@
                 destroy
             });
         }
+        function Thumb(_ref) {
+            let {swiper, extendParams, on} = _ref;
+            extendParams({
+                thumbs: {
+                    swiper: null,
+                    multipleActiveThumbs: true,
+                    autoScrollOffset: 0,
+                    slideThumbActiveClass: "swiper-slide-thumb-active",
+                    thumbsContainerClass: "swiper-thumbs"
+                }
+            });
+            let initialized = false;
+            let swiperCreated = false;
+            swiper.thumbs = {
+                swiper: null
+            };
+            function onThumbClick() {
+                const thumbsSwiper = swiper.thumbs.swiper;
+                if (!thumbsSwiper || thumbsSwiper.destroyed) return;
+                const clickedIndex = thumbsSwiper.clickedIndex;
+                const clickedSlide = thumbsSwiper.clickedSlide;
+                if (clickedSlide && dom(clickedSlide).hasClass(swiper.params.thumbs.slideThumbActiveClass)) return;
+                if ("undefined" === typeof clickedIndex || null === clickedIndex) return;
+                let slideToIndex;
+                if (thumbsSwiper.params.loop) slideToIndex = parseInt(dom(thumbsSwiper.clickedSlide).attr("data-swiper-slide-index"), 10); else slideToIndex = clickedIndex;
+                if (swiper.params.loop) {
+                    let currentIndex = swiper.activeIndex;
+                    if (swiper.slides.eq(currentIndex).hasClass(swiper.params.slideDuplicateClass)) {
+                        swiper.loopFix();
+                        swiper._clientLeft = swiper.$wrapperEl[0].clientLeft;
+                        currentIndex = swiper.activeIndex;
+                    }
+                    const prevIndex = swiper.slides.eq(currentIndex).prevAll(`[data-swiper-slide-index="${slideToIndex}"]`).eq(0).index();
+                    const nextIndex = swiper.slides.eq(currentIndex).nextAll(`[data-swiper-slide-index="${slideToIndex}"]`).eq(0).index();
+                    if ("undefined" === typeof prevIndex) slideToIndex = nextIndex; else if ("undefined" === typeof nextIndex) slideToIndex = prevIndex; else if (nextIndex - currentIndex < currentIndex - prevIndex) slideToIndex = nextIndex; else slideToIndex = prevIndex;
+                }
+                swiper.slideTo(slideToIndex);
+            }
+            function init() {
+                const {thumbs: thumbsParams} = swiper.params;
+                if (initialized) return false;
+                initialized = true;
+                const SwiperClass = swiper.constructor;
+                if (thumbsParams.swiper instanceof SwiperClass) {
+                    swiper.thumbs.swiper = thumbsParams.swiper;
+                    Object.assign(swiper.thumbs.swiper.originalParams, {
+                        watchSlidesProgress: true,
+                        slideToClickedSlide: false
+                    });
+                    Object.assign(swiper.thumbs.swiper.params, {
+                        watchSlidesProgress: true,
+                        slideToClickedSlide: false
+                    });
+                } else if (utils_isObject(thumbsParams.swiper)) {
+                    const thumbsSwiperParams = Object.assign({}, thumbsParams.swiper);
+                    Object.assign(thumbsSwiperParams, {
+                        watchSlidesProgress: true,
+                        slideToClickedSlide: false
+                    });
+                    swiper.thumbs.swiper = new SwiperClass(thumbsSwiperParams);
+                    swiperCreated = true;
+                }
+                swiper.thumbs.swiper.$el.addClass(swiper.params.thumbs.thumbsContainerClass);
+                swiper.thumbs.swiper.on("tap", onThumbClick);
+                return true;
+            }
+            function update(initial) {
+                const thumbsSwiper = swiper.thumbs.swiper;
+                if (!thumbsSwiper || thumbsSwiper.destroyed) return;
+                const slidesPerView = "auto" === thumbsSwiper.params.slidesPerView ? thumbsSwiper.slidesPerViewDynamic() : thumbsSwiper.params.slidesPerView;
+                const autoScrollOffset = swiper.params.thumbs.autoScrollOffset;
+                const useOffset = autoScrollOffset && !thumbsSwiper.params.loop;
+                if (swiper.realIndex !== thumbsSwiper.realIndex || useOffset) {
+                    let currentThumbsIndex = thumbsSwiper.activeIndex;
+                    let newThumbsIndex;
+                    let direction;
+                    if (thumbsSwiper.params.loop) {
+                        if (thumbsSwiper.slides.eq(currentThumbsIndex).hasClass(thumbsSwiper.params.slideDuplicateClass)) {
+                            thumbsSwiper.loopFix();
+                            thumbsSwiper._clientLeft = thumbsSwiper.$wrapperEl[0].clientLeft;
+                            currentThumbsIndex = thumbsSwiper.activeIndex;
+                        }
+                        const prevThumbsIndex = thumbsSwiper.slides.eq(currentThumbsIndex).prevAll(`[data-swiper-slide-index="${swiper.realIndex}"]`).eq(0).index();
+                        const nextThumbsIndex = thumbsSwiper.slides.eq(currentThumbsIndex).nextAll(`[data-swiper-slide-index="${swiper.realIndex}"]`).eq(0).index();
+                        if ("undefined" === typeof prevThumbsIndex) newThumbsIndex = nextThumbsIndex; else if ("undefined" === typeof nextThumbsIndex) newThumbsIndex = prevThumbsIndex; else if (nextThumbsIndex - currentThumbsIndex === currentThumbsIndex - prevThumbsIndex) newThumbsIndex = thumbsSwiper.params.slidesPerGroup > 1 ? nextThumbsIndex : currentThumbsIndex; else if (nextThumbsIndex - currentThumbsIndex < currentThumbsIndex - prevThumbsIndex) newThumbsIndex = nextThumbsIndex; else newThumbsIndex = prevThumbsIndex;
+                        direction = swiper.activeIndex > swiper.previousIndex ? "next" : "prev";
+                    } else {
+                        newThumbsIndex = swiper.realIndex;
+                        direction = newThumbsIndex > swiper.previousIndex ? "next" : "prev";
+                    }
+                    if (useOffset) newThumbsIndex += "next" === direction ? autoScrollOffset : -1 * autoScrollOffset;
+                    if (thumbsSwiper.visibleSlidesIndexes && thumbsSwiper.visibleSlidesIndexes.indexOf(newThumbsIndex) < 0) {
+                        if (thumbsSwiper.params.centeredSlides) if (newThumbsIndex > currentThumbsIndex) newThumbsIndex = newThumbsIndex - Math.floor(slidesPerView / 2) + 1; else newThumbsIndex = newThumbsIndex + Math.floor(slidesPerView / 2) - 1; else if (newThumbsIndex > currentThumbsIndex && 1 === thumbsSwiper.params.slidesPerGroup) ;
+                        thumbsSwiper.slideTo(newThumbsIndex, initial ? 0 : void 0);
+                    }
+                }
+                let thumbsToActivate = 1;
+                const thumbActiveClass = swiper.params.thumbs.slideThumbActiveClass;
+                if (swiper.params.slidesPerView > 1 && !swiper.params.centeredSlides) thumbsToActivate = swiper.params.slidesPerView;
+                if (!swiper.params.thumbs.multipleActiveThumbs) thumbsToActivate = 1;
+                thumbsToActivate = Math.floor(thumbsToActivate);
+                thumbsSwiper.slides.removeClass(thumbActiveClass);
+                if (thumbsSwiper.params.loop || thumbsSwiper.params.virtual && thumbsSwiper.params.virtual.enabled) for (let i = 0; i < thumbsToActivate; i += 1) thumbsSwiper.$wrapperEl.children(`[data-swiper-slide-index="${swiper.realIndex + i}"]`).addClass(thumbActiveClass); else for (let i = 0; i < thumbsToActivate; i += 1) thumbsSwiper.slides.eq(swiper.realIndex + i).addClass(thumbActiveClass);
+            }
+            on("beforeInit", (() => {
+                const {thumbs} = swiper.params;
+                if (!thumbs || !thumbs.swiper) return;
+                init();
+                update(true);
+            }));
+            on("slideChange update resize observerUpdate", (() => {
+                update();
+            }));
+            on("setTransition", ((_s, duration) => {
+                const thumbsSwiper = swiper.thumbs.swiper;
+                if (!thumbsSwiper || thumbsSwiper.destroyed) return;
+                thumbsSwiper.setTransition(duration);
+            }));
+            on("beforeDestroy", (() => {
+                const thumbsSwiper = swiper.thumbs.swiper;
+                if (!thumbsSwiper || thumbsSwiper.destroyed) return;
+                if (swiperCreated) thumbsSwiper.destroy();
+            }));
+            Object.assign(swiper.thumbs, {
+                init,
+                update
+            });
+        }
         function initSliders() {
-            if (document.querySelector(".interested__slider")) new core(".interested__slider", {
+            const exampleSliders = document.querySelectorAll(".examples__slider");
+            if (exampleSliders.length) exampleSliders.forEach((slide => {
+                const paging = slide.querySelector(".examples-pagination-bullets");
+                new core(slide, {
+                    modules: [ Pagination ],
+                    observer: true,
+                    observeParents: true,
+                    slidesPerView: 3,
+                    slidesPerGroup: 1,
+                    spaceBetween: 20,
+                    autoHeight: true,
+                    speed: 1500,
+                    loop: true,
+                    pagination: {
+                        el: paging,
+                        clickable: true
+                    },
+                    breakpoints: {
+                        320: {
+                            slidesPerView: 1,
+                            spaceBetween: 10,
+                            autoHeight: true,
+                            slidesPerGroup: 1
+                        },
+                        540: {
+                            slidesPerView: 1.2,
+                            slidesPerGroup: 1,
+                            spaceBetween: 15
+                        },
+                        768: {
+                            slidesPerView: 2,
+                            spaceBetween: 20,
+                            slidesPerGroup: 2
+                        },
+                        1025: {
+                            slidesPerView: 3,
+                            spaceBetween: 20,
+                            slidesPerGroup: 3
+                        }
+                    },
+                    on: {}
+                });
+            }));
+            if (document.querySelector(".gallery-details__slider")) new core(".gallery-details__slider", {
                 modules: [ Pagination ],
                 observer: true,
                 observeParents: true,
-                slidesPerView: 5,
-                spaceBetween: 26,
+                slidesPerView: 3,
+                slidesPerGroup: 1,
+                spaceBetween: 20,
                 autoHeight: true,
-                speed: 800,
+                speed: 1500,
                 loop: true,
                 pagination: {
-                    el: ".interested__swiper-pagination.swiper-pagination",
+                    el: ".gallery-details-pagination-bullets",
                     clickable: true
                 },
                 breakpoints: {
                     320: {
                         slidesPerView: 1,
-                        spaceBetween: 0,
+                        spaceBetween: 10,
                         autoHeight: true,
                         slidesPerGroup: 1
                     },
-                    468: {},
-                    640: {
-                        slidesPerView: 1,
-                        spaceBetween: 20,
-                        slidesPerGroup: 1
+                    540: {
+                        slidesPerView: 1.2,
+                        slidesPerGroup: 1,
+                        spaceBetween: 15
                     },
                     768: {
                         slidesPerView: 2,
                         spaceBetween: 20,
-                        slidesPerGroup: 1
+                        slidesPerGroup: 2
                     },
-                    992: {
+                    1025: {
                         slidesPerView: 3,
                         spaceBetween: 20,
-                        slidesPerGroup: 1
+                        slidesPerGroup: 3
+                    }
+                },
+                on: {}
+            });
+            if (document.querySelector(".introduce__slider")) new core(".introduce__slider", {
+                modules: [ Thumb, Pagination ],
+                observer: true,
+                observeParents: true,
+                slidesPerView: 1,
+                spaceBetween: 0,
+                autoHeight: true,
+                speed: 1e3,
+                loop: true,
+                touchRatio: .2,
+                thumbs: {
+                    swiper: {
+                        el: ".interested__slider",
+                        slidesPerView: 3
+                    }
+                },
+                pagination: {
+                    el: ".introduce-pagination-bullets",
+                    clickable: true
+                },
+                on: {}
+            });
+            if (document.querySelector(".interested__slider")) new core(".interested__slider", {
+                modules: [ Thumb ],
+                slidesPerView: 3,
+                spaceBetween: 32,
+                autoHeight: false,
+                speed: 500,
+                freeMode: true,
+                watchSlidesProgress: true,
+                breakpoints: {
+                    320: {
+                        slidesPerView: 1.2,
+                        spaceBetween: 0,
+                        autoHeight: false
                     },
-                    1150: {
-                        slidesPerView: 3,
-                        spaceBetween: 26,
-                        slidesPerGroup: 1
+                    541: {
+                        slidesPerView: 1.8,
+                        spaceBetween: 20
+                    },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 10
+                    },
+                    992: {
+                        slidesPerView: 2.7,
+                        spaceBetween: 20
                     },
                     1200: {
-                        slidesPerGroup: 1,
                         slidesPerView: 3,
-                        spaceBetween: 26
+                        spaceBetween: 30
                     }
                 },
                 on: {}
             });
         }
-        window.addEventListener("load", (function(e) {
+        window.addEventListener("DOMContentLoaded", (function(e) {
             initSliders();
         }));
         var lazyload_min = __webpack_require__(732);
